0989fa945e9e0d4ff5a3e194138649c2e5382520
Larry Custodio <custodioly@gmail.com>
2017-10-02 15:36:01 -0700

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,24 @@
-// write your code here
+/* 
+The following tasks are done  automate the build and reloading dev webserver:
+- Uglify CSS & JS
+- Concatenate Files
+*/
+
+const gulp = require("gulp");
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const nodemon = require('gulp-nodemon');
+
+gulp.task("build:js",()=>{
+    return gulp.src("src/js/*.js")
+    .pipe(uglify())
+    .pipe(concat('bundle.min.js'))
+    .pipe(gulp.dest("dist"))
+});
+
+gulp.watch();
+
+//
 
 module.exports = gulp;
